5fb219c0e183d69e422fe6ac48c55e61f6efc249
JitenMehta01 <40121263+JitenMehta01@users.noreply.github.com>
2021-05-08 20:05:24 +0100

    add all pug/routes/errHandlers and static asssets. All is connected and working

--- errhandlers/errHandlers.js
+++ errhandlers/errHandlers.js
@@ -1,15 +1,15 @@
 const fourOhfourErrHandlres = (req, res, next) =>{
     const err = new Error();
     err.message = `oops you've hit a . This page does not exist!`
-    res.status(404).render('not-found', {err});
+    res.status(404).render('page-not-found', {err});
 }
 
 const globalErrHandler = (err,req, res, next) =>{
     if(err.satus === 404){
         res.status(404).render('not-found', {err});
     } else{
-        err.message = err.message || `Ooops something has gone wrong!`;
-        res.status(err.status || 500).render('not found', {err})
+        err.message = err.message || `Ooops something has gone wrong! This maybe a server issue. Please be patient, we are working very hard to fix this`;
+        res.status(err.status || 500).render('error', {err})
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router= express.Router();
+const {projects_data} = require('../data/data.json'); 
+
 
 // Home
 router.get('/', (req, res) =>{
-    res.render('index');
+    res.render('index', {projects_data});
 })
 
 module.exports = router;
